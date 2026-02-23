5497c95dd461225e6b8696f1215c533ae8ca4f22
Ellsworth McCullough <ellsworthmmcc@outlook.com>
2026-02-22 18:21:20 -0800

    feat: initial blog

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Example Author",
+        "title": "Examle Title",
+        "content": "Example Content",
+        "date_posted": "January 1, 2020",
+    },
+    {
+        "id": 2,
+        "author": "Example Author",
+        "title": "Examle Title",
+        "content": "Example Content",
+        "date_posted": "January 1, 2020",
+    },
+]
+
+
+@app.get("/", include_in_schema=False)
+@app.get("/posts", include_in_schema=False)
+def home(request: Request):
+    return templates.TemplateResponse(request=request, name="home.html", context={"posts": posts, "title": "Home"})
+
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
