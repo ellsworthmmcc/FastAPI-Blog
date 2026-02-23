2c4a71ac9db381855cea0a3bd83680dfe29b497b
Ellsworth McCullough <ellsworthmmcc@outlook.com>
2026-02-22 18:36:51 -0800

    refactor: adjusted formatting

--- main.py
+++ main.py
@@ -26,10 +26,14 @@ posts: list[dict] = [
 ]
 
 
-@app.get("/", include_in_schema=False)
-@app.get("/posts", include_in_schema=False)
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
 def home(request: Request):
-    return templates.TemplateResponse(request=request, name="home.html", context={"posts": posts, "title": "Home"})
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html",
+        context={"posts": posts, "title": "Home"}
+        )
 
 
 @app.get("/api/posts")
